fa83ab03a7d4d90ca52754575dc9535c1263b736
bilgejava6 <bilgejava6@gmail.com>
2024-09-26 11:25:17 +0300

    Uygulama Soruları.

--- src/main/java/com/sorular/Runner.java
+++ src/main/java/com/sorular/Runner.java
@@ -16,6 +16,28 @@ public class Runner {
          * 3 adet personel, 1 adet müdür personel, 2 departman ve her personel için en az 2 adres tanımlatyın.
          *
          * -
+         *
+         * Sorular
+         * 1- Adı verilen bir departmanda çalışan personelleri listesi - (ad  > like  + ignoreCase)
+         * 2- Belli bir ilde ikamet eden personellerin listesi
+         * 3- Adını verdiğim Müdür ile çalışan personellerin listesi
+         * 4- Belli bir ünvana sahip personellerin listesi.
+         * 5- departmanlarda çalışan personellerin sayıları -> bilgiişlem: 5, insankaynakları: 3 gibi
+         * 6- ünvanlarına göre personel sayıları
+         * 7- personel Listesi;
+         * id: 44
+         * ad: Muhammet
+         * ünvan: Eğitmen
+         * departmanı: Yazılım
+         * müdür: Ayhan
+         * -------------
+         * şeklinde bir çıktı verin.
+         *
+         * ÖRN:
+         * public List<VwPersonel> findAllViewPersonel(){
+         *     return null;
+         * }
+         *
          */
     }
 }
